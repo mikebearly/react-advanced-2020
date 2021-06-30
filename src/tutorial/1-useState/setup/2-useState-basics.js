2df93b2e7117d37e70fdefc9db8da3d775bde821
Navis Michael Bearly J <navis.programmer@gmail.com>
2021-07-01 01:16:18 +0530

    Advanced react until useState-array

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState("random text");
+
+  const handleClick = () => {
+    if (text === "random text") {
+      setText("hello world");
+    } else {
+      setText("random text");
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <h2>{text}</h2>
+      <button type="button" className="btn" onClick={handleClick}>
+        Change title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
