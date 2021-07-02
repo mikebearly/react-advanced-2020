ec7201c00e395f2e27e202dda27753fd0f288839
Navis Michael Bearly J <navis.programmer@gmail.com>
2021-07-02 21:18:11 +0530

    useEffect dependency completion

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,26 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    console.log('call useeffect');
+    if (value > 0) {
+      document.title = `Messages(${value})`;
+    }
+  }, [value]);
+
+  useEffect(() => {
+    console.log('Hello World');
+  }, []);
+  console.log('render component');
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        Click Me
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
