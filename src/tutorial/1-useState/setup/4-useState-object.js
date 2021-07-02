d7631e73933b7216b45f59555481b5bc6071d5cb
Navis Michael Bearly J <navis.programmer@gmail.com>
2021-07-02 16:51:26 +0530

    useState

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,30 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: 'Peter',
+    age: 31,
+    message: 'random message',
+    location: 'India',
+  });
+  const handleChange = () => {
+    setPerson({
+      ...person,
+      message: 'Hello World will work',
+      location: 'US',
+    });
+  };
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <h3>{person.location}</h3>
+      <button className="btn" onClick={handleChange}>
+        Change Message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,42 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+  const reset = () => {
+    setValue(0);
+  };
+  const increaseCounter = () => {
+    setTimeout(() => {
+      setValue((prevValue) => {
+        return prevValue + 1;
+      });
+    }, 2000);
+    // setValue(value + 1);
+  };
+  return (
+    <>
+      <section style={{ margin: '4rem 0' }}>
+        <h2>regular counter</h2>
+        <h4>{value}</h4>
+        <button className="btn" onClick={() => setValue(value - 1)}>
+          Decrease
+        </button>
+        <button className="btn" onClick={reset}>
+          Reset
+        </button>
+        <button className="btn" onClick={() => setValue(value + 1)}>
+          Increase
+        </button>
+      </section>
+      <section style={{ margin: '4rem 0' }}>
+        <h2>complex counter</h2>
+        <h4>{value}</h4>
+        <button className="btn" onClick={increaseCounter}>
+          increase later
+        </button>
+      </section>
+    </>
+  );
 };
 
 export default UseStateCounter;
