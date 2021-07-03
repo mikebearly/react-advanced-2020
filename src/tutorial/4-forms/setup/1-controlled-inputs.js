6024e7ac7005a013d01cebddb2117111c1e43909
Navis Michael Bearly J <navis.programmer@gmail.com>
2021-07-03 20:07:19 +0530

    Add items to the list

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,7 +6,65 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const [firstName, setFirstName] = useState('');
+  const [email, setEmail] = useState('');
+  const [people, setPeople] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let newPerson = { id: new Date().getTime().toString(), firstName, email };
+    if (firstName && email) {
+      setPeople((oldPeople) => {
+        return [...oldPeople, newPerson];
+      });
+    }
+    setFirstName('');
+    setEmail('');
+    console.log(firstName, email);
+  };
+
+  return (
+    <>
+      <article>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="firstName">First Name</label>
+            <input
+              type="text"
+              name="firstName"
+              id="firstName"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <button type="submit">Add Person</button>
+          </div>
+        </form>
+      </article>
+      <div>
+        {people.map((person) => {
+          const { id, firstName, email } = person;
+          return (
+            <div className="item" key={id}>
+              <h4>{firstName}</h4>
+              <p>{email}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default ControlledInputs;
