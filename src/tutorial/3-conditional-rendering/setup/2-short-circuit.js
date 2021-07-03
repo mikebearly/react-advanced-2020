950c7ad3558e3f13fb3cebe426807d22d670293d
Navis Michael Bearly J <navis.programmer@gmail.com>
2021-07-03 18:18:37 +0530

    show/hide useEffect cleanup

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -4,14 +4,23 @@ import React, { useState } from 'react';
 
 const ShortCircuit = () => {
   const [text, setText] = useState('');
-  const firstValue = text || 'hello world';
-  const secondValue = text && 'hello world';
+  const [isError, setIsError] = useState(false);
+  // const firstValue = text || 'hello world';
+  // const secondValue = text && 'hello world';
 
   return (
     <>
-      <h1>short circuit</h1>
+      {/*<h1>short circuit</h1>
       <h2>1st Value: {firstValue}</h2>
-      <h2>2nd Value: {secondValue}</h2>
+      <h2>2nd Value: {secondValue}</h2>*/}
+      <h1>{text || 'Navis'}</h1>
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        Toggle Error
+      </button>
+      {/* text && 'Hello World' */}
+      {/* !text && 'Hello World' */}
+      {isError && <h3>Error...</h3>}
+      {isError ? <h3>Error...</h3> : <h3>this is no error</h3>}
     </>
   );
 };
